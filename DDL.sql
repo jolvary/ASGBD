31ee44cfe47d7e49b5ac004589c2d7ff0944e68a
jolvary <jolvary@gmail.com>
2022-01-27 12:34:17 +0100

    DDL v1

---
+++ DDL.sql
@@ -0,0 +1,23 @@
+drop user ACME cascade;
+create user ACME identified by acme;
+grant create session to ACME;
+grant create table to ACME;
+grant unlimited tablespace to ACME;
+
+conn ACME/acme
+
+create table CLIENTES (
+	idCli number,
+	nomCli varchar2(20),
+	dniCli number,
+	constraint pk_Cli primary key (idCli),
+	constraint uq_Cli unique (dniCli)
+);
+
+create table VISITAS (
+	idVis number,
+	cliente varchar2(50),
+	ubi varchar2(50),
+	constraint pk_Vis primary key (idMap),
+    constraint fk_Cli foreign key (cliente) references CLIENTES (idCli),
+);
