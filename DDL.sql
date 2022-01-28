9090617d4834c50a5152b83a2db77fe474c148c0
jolvary <jolvary@gmail.com>
2022-01-28 09:11:37 +0100

    DDL v12

--- DDL.sql
+++ DDL.sql
@@ -22,7 +22,7 @@ grant create user, drop user to CEO;
 
 create role Jefatura_Tecnica;
 grant create session to Jefatura_Tecnica;
-grant create table, create synonym, create view to Jefatura_Tecnica;
+grant create table, create public synonym, create view to Jefatura_Tecnica;
 
 create role Jefatura_Comercial;
 grant create session to Jefatura_Comercial;
